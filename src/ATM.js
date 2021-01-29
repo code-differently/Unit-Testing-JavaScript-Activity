8100db3c4cdbb3f678b7da9e719fd8d0cd732188
Amarachukwu Ugoanusi <amarachukwuugoanusi@AmarachukwusAir.fios-router.home>
2021-01-29 00:10:27 -0500

    commit with some solutions

--- src/ATM.js
+++ src/ATM.js
@@ -1,4 +1,22 @@
 
 class ATM {
+    constructor(amount = 0) {
+        this.amount = amount;
+    }
+    getCurrentBalance(){
+        return this.amount;
+    }
+    withdraw(amountToWithdraw){
 
+     this.amount = this.amount - amountToWithdraw;
+     //check if you've overdrafted
+     return this.amount > 0 ? amountToWithdraw : null;
+    }
+    deposit(amountTodeposit){
+
+        this.amount = this.amount + amountTodeposit;
+        //check if you've overdrafted
+       
+       }
 }
+module.exports = ATM;
