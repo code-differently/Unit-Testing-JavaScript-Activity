575c2b701854a80f0977069be811725a7b90d4a0
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-02-01 19:16:22 -0500

    first commit

--- src/Tesla.js
+++ src/Tesla.js
@@ -1,4 +1,31 @@
 
+
+//How many more miles can I drive before I need battery charge
+//Battery charge percentage
+
 class Tesla {
-    
+    constructor(charge, miles) {
+        this.tesla;
+        this.batteryCharge = charge;
+        this.howManyMiles = miles;
+    }
+    batteryCharged() {
+        console.log(this.batteryCharge);
+        return this.batteryCharge;
+    }
+    validateBatteryCharge() {
+        if(this.batteryCharge >= 0 && this.batteryCharge <= 100){
+          return true;
+        } else return false;
+    }
+    milesToGo() {
+        return this.howManyMiles;
+    }
+    validateMilesToGo() {
+        if(this.howManyMiles >= 0 && this.howManyMiles <= 400){
+            return true;
+        } else return false;
+    }
+
 }
+module.exports = Tesla;
